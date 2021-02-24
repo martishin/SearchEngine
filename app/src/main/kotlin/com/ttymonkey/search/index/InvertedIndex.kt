c6d0e417f6697464caf83ad36720a69b28178544
Aleksei Martishin <alekseim@amazon.com>
2021-02-24 21:51:57 +0100

    remove DocumentId->Document mapping from InvertedIndex

--- app/src/main/kotlin/com/ttymonkey/search/index/InvertedIndex.kt
+++ app/src/main/kotlin/com/ttymonkey/search/index/InvertedIndex.kt
@@ -5,37 +5,22 @@ import kotlin.concurrent.read
 import kotlin.concurrent.write
 import kotlinx.serialization.*
 import java.io.File
-import java.util.concurrent.atomic.AtomicInteger
 
 typealias Position = Int
-typealias DocumentId = Int
 typealias Document = String
 
 @Serializable
 class InvertedIndex {
-    private var index: MutableMap<String, MutableMap<DocumentId, MutableList<Position>>> = hashMapOf()
-    private val documentsIndex: MutableMap<DocumentId, Document> = hashMapOf()
-
-    @Transient
-    private val currentDocumentId: AtomicInteger = AtomicInteger(1)
+    private var index: MutableMap<String, MutableMap<Document, MutableList<Position>>> = hashMapOf()
 
     @Transient
     private val lock = ReentrantReadWriteLock()
 
     fun addTokens(file: File, tokens: List<String>) = lock.write {
-        val documentId = currentDocumentId.getAndIncrement()
-        documentsIndex[documentId] = file.path
-
         tokens.forEachIndexed { idx, token ->
-            if (!index.containsKey(token)) {
-                index[token] = hashMapOf()
-            }
-
-            if (index[token]!!.containsKey(documentId)) {
-                index[token]!![documentId]?.add(idx + 1)
-            } else {
-                index[token]!![documentId] = mutableListOf(idx + 1)
-            }
+            index.getOrPut(token, { hashMapOf() })
+                    .getOrPut(file.path, { mutableListOf() })
+                    .add(idx + 1)
         }
     }
 
@@ -47,13 +32,13 @@ class InvertedIndex {
         var previousTokenToDocuments = getTokenPositions(tokens[0])
         for (idx in 1 until tokens.size) {
             val currentTokenToDocuments = getTokenPositions(tokens[idx])
-            val continuousTokenPositions : MutableMap<DocumentId, MutableList<Position>> = hashMapOf()
+            val continuousTokenPositions : MutableMap<Document, MutableList<Position>> = hashMapOf()
 
             val documentsIntersection = previousTokenToDocuments.keys.intersect(currentTokenToDocuments.keys)
 
-            documentsIntersection.forEach { documentId ->
-                val previousPositions = previousTokenToDocuments.getValue(documentId)
-                val currentPositions = currentTokenToDocuments.getValue(documentId)
+            documentsIntersection.forEach { document ->
+                val previousPositions = previousTokenToDocuments.getValue(document)
+                val currentPositions = currentTokenToDocuments.getValue(document)
 
                 var ppPointer = 0
                 var cpPointer = 0
@@ -61,11 +46,11 @@ class InvertedIndex {
                 while (ppPointer < previousPositions.size && cpPointer < currentPositions.size) {
                     when {
                         currentPositions[cpPointer] == previousPositions[ppPointer] + 1 -> {
-                            if (!continuousTokenPositions.containsKey(documentId)) {
-                                continuousTokenPositions[documentId] = mutableListOf()
+                            if (!continuousTokenPositions.containsKey(document)) {
+                                continuousTokenPositions[document] = mutableListOf()
                             }
 
-                            continuousTokenPositions[documentId]?.add(currentPositions[cpPointer])
+                            continuousTokenPositions[document]?.add(currentPositions[cpPointer])
                             ++ppPointer
                             ++cpPointer
                         }
@@ -82,15 +67,12 @@ class InvertedIndex {
             previousTokenToDocuments = continuousTokenPositions
         }
 
-        val documentsPositions : MutableMap<Document, List<Position>> = hashMapOf()
-        previousTokenToDocuments.forEach { (documentId, positions) ->
-            documentsPositions[documentsIndex[documentId]!!] = positions.map{ it - tokens.size + 1  }
+        return previousTokenToDocuments.mapValues { positions ->
+            positions.value.map{ it - tokens.size + 1  }
         }
-
-        return documentsPositions
     }
 
-    private fun getTokenPositions(token: String): Map<DocumentId, List<Position>>{
+    private fun getTokenPositions(token: String): Map<Document, List<Position>> = lock.read {
         return index[token] ?: hashMapOf()
     }
 }
