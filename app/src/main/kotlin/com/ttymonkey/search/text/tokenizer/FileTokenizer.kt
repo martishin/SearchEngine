d84fbc6963fdfe3ba041f34329d524e788bb58c3
Aleksei Martishin <alekseim@amazon.com>
2021-02-24 12:39:43 +0100

    Use forEachLine instead of BufferedReader#readLine

--- app/src/main/kotlin/com/ttymonkey/search/text/tokenizer/FileTokenizer.kt
+++ app/src/main/kotlin/com/ttymonkey/search/text/tokenizer/FileTokenizer.kt
@@ -1,9 +1,7 @@
 package com.ttymonkey.search.text.tokenizer
 
 import mu.KotlinLogging
-import java.io.BufferedReader
 import java.io.File
-import java.io.FileReader
 import java.io.IOException
 
 private val log = KotlinLogging.logger {}
@@ -11,17 +9,10 @@ private val log = KotlinLogging.logger {}
 object FileTokenizer {
     fun tokenize(file: File): List<String> {
         val tokens: MutableList<String> = mutableListOf()
-        val reader: BufferedReader
         try {
-            reader = BufferedReader(FileReader(file))
-            var line = reader.readLine()
-
-            while (line != null) {
-                tokens += SpaceTokenizer.tokenize(line)
-                line = reader.readLine()
+            file.forEachLine {
+                tokens += SpaceTokenizer.tokenize(it)
             }
-
-            reader.close()
         } catch (e: IOException) {
             log.error("Error happened when reading from file {}", file.path, e)
         }
